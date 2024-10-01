28797eee2ad4c90c3698553d1b5741b8875ede3d
devin123123 <143128348+devin123123@users.noreply.github.com>
2024-09-30 23:58:07 -0400

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,9 @@
+
+
+fruits = ['watermelon', 'kiwi', 'orange', 'clementine']
+
+for fruit in fruits:
+    if fruit == "orange":
+        print(fruit.capitalize())  # Using capitalize method for orange
+    else:
+        print(fruit.upper())  # Using upper method for the rest of the fruits
