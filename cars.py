b8b7712d43fa4fa22380e788f9b4b9c50ee7e566
devin123123 <143128348+devin123123@users.noreply.github.com>
2024-10-09 10:42:18 -0400

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,7 @@
+meals = ['Sesame Chicken', 'Fish N Chips', 'Spaghetti', 'Pizza']
+
+for meal in meals:
+    if meal == "Spaghetti":
+        print(meal.title()) # you may use any string method you wish.
+    else:
+        print(meal.upper()) # you man use any string method you wish
