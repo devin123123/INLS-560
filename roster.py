ea9ab2dd388c03fb7807824ceec5ed600162f312
devin123123 <143128348+devin123123@users.noreply.github.com>
2024-11-11 22:45:38 -0500

    Add files via upload

--- roster.py
+++ roster.py
@@ -1,8 +1,9 @@
 # https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
 roster = ['Tyson', 'Davis', 'Cadeau']
 
+data = pd.DataFrame(roster)
+print(data)
 
-for player in roster:
-    print(player)
 
