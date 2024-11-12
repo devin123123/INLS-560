1c84aef91d8d578c5a9644ca9a688bd5e7044552
devin123123 <143128348+devin123123@users.noreply.github.com>
2024-11-11 22:52:56 -0500

    Delete roster.py

--- roster.py
+++
@@ -1,9 +0,0 @@
-# https://goheels.com/sports/mens-basketball/roster
-import pandas as pd
-
-roster = ['Tyson', 'Davis', 'Cadeau']
-
-data = pd.DataFrame(roster, columns=['Last Name'])
-print(data)
-
-
