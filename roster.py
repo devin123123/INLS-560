2bd18dd88c8b375e863c360c9c5d210fed0d5f79
devin123123 <143128348+devin123123@users.noreply.github.com>
2024-11-11 23:58:15 -0500

    Add files via upload

--- roster.py
+++ roster.py
@@ -1,12 +1,22 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-player = {"Last Name": ["Tyson", "Davis", "Cadeau"],
-          "First Name": ["Cade", "RJ", "Elliot"], 
-          "height": [79, 72, 73], 
-          "weight": [200,180,180]}
-data = pd.DataFrame(player)
+# Creating a dictionary for the players with the required data
+players = {
+    "Last Name": ["Tyson", "Davis", "Cadeau", "Claude", "Brown", "Davis", "Trimble", "Powell", "Jackson", "Washington"],
+    "First Name": ["Cade", "RJ", "Elliot", "Tyzhaun", "James", "Elijah", "Seth", "Drake", "Ian", "Jalen"],
+    "Height (in)": [79, 72, 73, 79, 82, 75, 75, 78, 76, 82],
+    "Weight (lbs)": [200, 180, 180, 230, 215, 215, 195, 195, 190, 235]
+}
+
+data = pd.DataFrame(players)
+
+# Calculate BMI and round it to 2 decimal points
+data["BMI"] = ((data["Weight (lbs)"] / 2.205) / ((data["Height (in)"] / 39.37) ** 2)).round(2)
+
+# Print the DataFrame
 print(data)
+
 
 
 
